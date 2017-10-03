14f22e19bdc8ebe437cc923491d78f759b27e257
Shahriar Shakil <30352466+shahriar787@users.noreply.github.com>
2017-10-04 04:59:07 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//var number = prompt("Enter a number");
+//if(number>2){
+//    document.write("Your Entered Number is greater then 2.")
+//}else{
+//    document.write("Your Entered Number is Less then 2.")
+//}
+
+
+var number1 = Number(prompt("Enter first Number"));
+var number2 = Number(prompt("Enter second Number"));
+
+var result = number1 + number2;
+document.write("Your Calculation Result is: " + result);
